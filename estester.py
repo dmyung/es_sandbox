198ac04ea61e425a4520195b00e74611d531ba89
dmyung <dmyung@dimagi.com>
2012-11-07 23:00:30 -0500

    adding new mapping types (not much differernce)

--- estester.py
+++ estester.py
@@ -10,9 +10,26 @@ def get_es():
     return rawes.Elastic('%s:%s' % (ES_HOST, ES_PORT))
 
 index_mappings = {
-'nothing': None,
-'nodate': {'nodate': {'date_detection': False}},
-#typed...
+    'nothing': None,
+    'nodate': {'nodate': {'date_detection': False}},
+    'dynamic_date': {
+        'dynamic_date': {
+            "dynamic_date_formats" : ["yyyy-MM-dd", "dd-MM-yyyy",
+                "date_optional_time", ] #"" doesn't work
+            }
+        },
+    'dynamic_all': {
+        'dynamic_all':  {
+            "dynamic_templates" : [ 
+                {
+                    "store_generic" : {
+                        "match" : "*",
+                        "mapping": { "store" : "yes" }
+                        }
+                    }
+                ]
+            }
+        }
 }
 
 def main():
@@ -26,13 +43,13 @@ def main():
             es.delete(index)
         es.put(index)
         if mapping is not None:
-            es.put("%s/%s/_mapping" % (index, ES_TYPE), data=mapping)
+            print es.put("%s/%s/_mapping" % (index, index), data=mapping)
         for x in files:
             if x.endswith('.json'):
                 filenum = int(x[0:3])
                 success=True
                 with open(x, 'r') as fin:
-                    res = es.put("%s/%s/%d" % (index, ES_TYPE, filenum), data=simplejson.loads(fin.read()))
+                    res = es.put("%s/%s/%d" % (index, index, filenum), data=simplejson.loads(fin.read()))
                     if res.get('status',None) == 400:
                         success=False
                 print "%s, %s => %s" % (index, x, 'SUCCESS' if success else
